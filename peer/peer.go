7e18fd9e43fb035f1e4565b87a92c09b492c6eea
Ben Meier <ben.meier@oracle.com>
2019-10-27 17:54:21 +0000

    Example of UDP broadcast in Golang

---
+++ peer/peer.go
@@ -0,0 +1,88 @@
+// This main file shows an example of UDP broadcast and receive.
+//
+// It should be run like
+//
+//    docker run --rm -ti -v "$(pwd):/cwd" golang:1.13 go run /cwd/peer/peer.go
+//
+// When running just one instance of this program, you should see the peer receiving its own broadcasts.
+// When running multiple copies of this program, you should see each peer receiving each other peers messages.
+
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net"
+	"strconv"
+	"time"
+)
+
+// We sent to, and listen on, the same port.
+const commonPort = 2002
+
+func main() {
+	// Construct a random peer name for myself which I'll broadcast over the network
+	rand.Seed(time.Now().UnixNano())
+	name := "peer-" + strconv.Itoa(rand.Int())
+	log.Printf("I am %s", name)
+
+	// Resolve broadcast address into a address object for udp network
+	log.Printf("Resolving broadcast address..")
+	bCastAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:" + strconv.Itoa(commonPort))
+	if err != nil {
+		log.Fatalf("Error: Failed to resolve broadcast address: %s", err.Error())
+	}
+	log.Printf("Resolved %s.", bCastAddr)
+
+	// Background thread
+	go func() {
+		// "Connect" to udp address (assigns local source port)
+		log.Printf("Dialing broadcast address..")
+		conn, err := net.DialUDP("udp", nil, bCastAddr)
+		if err != nil {
+			log.Fatalf("Error: Failed to dial: %s", err.Error())
+		}
+		// Start sending packets off into the ether every second
+		for {
+			time.Sleep(time.Second)
+			log.Printf("Broadcasting..")
+			if _, err := conn.Write([]byte(name)); err != nil {
+				log.Printf("Error: %s", err.Error())
+			}
+			// We could choose to listen here on our source port and wait for direct replies
+			// but since we want to listen on the broadcast port ourselves and don't actually expect
+			// any direct replies, we wont.
+		}
+	}()
+
+	// Resolve the broadcast listen address on our own interface
+	log.Printf("Resolving listen address..")
+	listenAddr, err := net.ResolveUDPAddr("udp", ":" + strconv.Itoa(commonPort))
+	if err != nil {
+		log.Fatalf("Error: Failed to resolve listen address: %s", err.Error())
+	}
+	log.Printf("Resolved %s.", listenAddr)
+
+	// Now begin listing on the port, we'll
+	log.Printf("Beginning to listen..")
+	conn, err := net.ListenUDP("udp", listenAddr)
+	if err != nil {
+		log.Fatalf("Error: Failed to listen: %s", err.Error())
+	}
+	defer conn.Close()
+
+	// Buffer for reading received datagrams into, this is really up to the protocol and since in this case
+	// we know we're only sending short strings, we'll just use 1K. Remember, UDP datagrams can be fragmented
+	// into multiple packets.
+	buf := make([]byte, 1024)
+	for {
+		// Read some bytes from our listened connection and get the source addr. If we wanted to, we could write
+		// bytes directly back to that source address (if it was waiting to read on that)
+		n, addr, err := conn.ReadFromUDP(buf)
+		log.Printf("Received '%s' from %s", string(buf[0:n]), addr)
+		if err != nil {
+			log.Printf("Error: %s", err.Error())
+		}
+	}
+
+}
